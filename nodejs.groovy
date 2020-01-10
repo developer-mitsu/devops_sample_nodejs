1dfeac781c5fc289cd33ed07725ec5737d6f920e
Mitsu <developer.mitsu@gmail.com>
2020-01-10 15:27:08 +0900

    add DSL

---
+++ nodejs.groovy
@@ -0,0 +1,19 @@
+job('NodeJS job') {
+	scm {
+		git('https://github.com/developer-mitsu/devops_sample_nodejs.git') { node ->
+			node / gitConfigName('DSL User')
+			node / gitConfigEmail('jenkins-dsl@example.com')
+		}
+	}
+	trigger {
+		scm('H/5 * * * *')
+	}
+	wrappers {
+		nodejs('nodejs_v13')
+	}
+	steps {
+		shell("npm install")
+		shell("npm test")
+	}
+}
+
